b82b15d2cd3c2008314c5396c0cd0fb0b5342240
MathiasB25 <mathiasbdev@gmail.com>
2023-11-30 14:57:36 -0300

    fix modal

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -9,8 +9,8 @@ export default function Modal({ children, handleClose }) {
             whileInView={{ opacity: 1 }}
             exit={{ opacity: 0 }}
         >
-            <div className={"relative px-6 pb-6 pt-9 bg-background rounded-md mx-3"}>
-                <button onClick={handleClose} className={"absolute top-2 right-3"}>
+            <div className={"relative px-6 pb-6 pt-9 rounded-md mx-3"} style={{ position: "relative", background: "#141418", borderRadius: "0.375rem" }}>
+                <button onClick={handleClose} className={"absolute top-2 right-3"} style={{ position: "absolute", top: "0.5rem", right: "0.75rem" }}>
                     <i className="fa-sharp fa-regular fa-xmark text-xl"></i>
                 </button>
                 {children}
