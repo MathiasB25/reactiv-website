9dccd49628683f8fdc934dfbcb99b0de18bf1664
MathiasB25 <mathiasbdev@gmail.com>
2023-11-30 14:53:26 -0300

    fix modal

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -3,8 +3,8 @@ import { motion } from "framer-motion"
 export default function Modal({ children, handleClose }) {
     return (
         <motion.div 
-            className={"fixed inset-0 w-screen h-screen bg-[rgba(0,0,0,.9)] grid place-content-center z-10"}
-            style={{ position: "fixed" }}
+            className={"fixed inset-0 w-screen h-screen flex items-center justify-center z-10"}
+            style={{ position: "fixed", background: "rgba(0,0,0,.9)" }}
             initial={{ opacity: 0 }}
             whileInView={{ opacity: 1 }}
             exit={{ opacity: 0 }}
