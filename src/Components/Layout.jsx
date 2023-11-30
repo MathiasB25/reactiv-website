c90edae859b7a32ab0d05af3163ae88444eecbc9
MathiasB25 <mathiasbdev@gmail.com>
2023-11-30 14:37:59 -0300

    fix bg

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 
 export default function Layout({title, children}) {
     return (
-        <div className={"bg-background"}>
+        <div className={"bg-[#141418]"}>
             <Head>
                 <title>{title} | Reactiv</title>
             </Head>
